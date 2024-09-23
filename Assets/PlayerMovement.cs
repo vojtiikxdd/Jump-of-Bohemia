dd9bcb2aa91452c99d0fbfdd89055a1b40cd2def
vojtiikkxdd <vojtarazima@gmail.com>
2024-09-23 15:15:38 +0200

    adding platrom to test movement and starting to create a movement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float horizontalSpeed;
+    private float speed = 8.0f;
+    private float jumpingPower = 16.0f;
+    private bool isFacingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
