010f4f16876b60c36f9bf206f84c914be3c3f1a8
vojtiikkxdd <vojtarazima@gmail.com>
2024-10-30 11:34:46 +0100

    fixing code

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     private void GameMenu()
     {
-         scenemanagement.LoadScene(scenemanager.GetActiveScene().buildIndex + 1);
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void QuitGame()
