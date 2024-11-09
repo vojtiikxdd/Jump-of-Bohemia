8078491005d1363065bdacd6f381ca8dab51a2c2
majko13 <miklovicm.07@spst.eu>
2024-11-09 14:41:56 +0100

    Background sounds cahniging on player s Y

--- Assets/Scripts/BgSoundForest.cs
+++ Assets/Scripts/BgSoundForest.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class PlayerSounds : MonoBehaviour
-{
-    public AudioSource audioSource;
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.Play();
-    }
+// public class PlayerSounds : MonoBehaviour
+// {
+//     public AudioSource audioSource;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         audioSource = GetComponent<AudioSource>();
+//         audioSource.Play();
+//     }
 
 
-    public void StopMusic()
-    {
-        audioSource.Stop();
-    }
+//     public void StopMusic()
+//     {
+//         audioSource.Stop();
+//     }
 
-    public void SetVolume(float volume)
-    {
-        audioSource.volume = volume;
-    }
+//     public void SetVolume(float volume)
+//     {
+//         audioSource.volume = volume;
+//     }
    
-}
+// }
