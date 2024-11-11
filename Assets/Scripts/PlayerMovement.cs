b3e24d2ef2af375f9bc8b2669bd0f27a15059bf8
vojtiikkxdd <vojtarazima@gmail.com>
2024-11-11 20:12:45 +0100

    everything, just in case of going back

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,83 +4,112 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 6.0f;
-    private float jumpingPower = 14f;
-    public float maxJumpTime = 0.5f;
-    public float jumpForceMultiplier = 5f;
-    private bool canJump = false;
-    private float jumpTimer = 0f;
-    private bool isFacingRight = true;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-    public float groundCheckRadius = 0.1f;
-
-    private PlayerSoundJump playerSoundJump; // Reference to the PlayerSoundJump script
+    public float walkSpeed;
+    private float moveInput;
+    public bool isGrounded;
+    private Rigidbody2D rb;
+    public LayerMask groundMask;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        playerSoundJump = GetComponent<PlayerSoundJump>(); // Get the PlayerSoundJump script
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
+    void Update() {
+        moveInput = Input.GetAxis("Horizontal");
 
-        // Update grounded status
-        bool isGroundedFlag = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
 
-        // Reset canJump only when the player is grounded
-        if (isGroundedFlag && !canJump)
-        {
-            canJump = false;
-        }
-
-        // Start jump if grounded and space is pressed
-        if (Input.GetKeyDown(KeyCode.Space) && isGroundedFlag)
-        {
-            canJump = true;
-            jumpTimer = 0f;
-        }
-
-        // Track jump hold time
-        if (Input.GetKey(KeyCode.Space) && canJump)
-        {
-            jumpTimer += Time.deltaTime;
-            jumpTimer = Mathf.Clamp(jumpTimer, 0f, maxJumpTime);
-        }
-
-        // Release jump and play sound
-        if (Input.GetKeyUp(KeyCode.Space) && canJump)
-        {
-            Jump();
-            playerSoundJump.PlayJumpSound(); // Play the jump sound
-            canJump = false;
-        }
-
-        Flip(); 
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.5f, groundMask);
     }
 
-    private void Jump()
-    {
-        float totalJumpForce = jumpingPower + jumpForceMultiplier * (jumpTimer / maxJumpTime);
-        rb.AddForce(Vector3.up * totalJumpForce, ForceMode2D.Impulse);
-    }
+    // private float horizontal;
+    // private float speed = 6.0f;
+    // private float jumpingPower = 14f;
+    // public float maxJumpTime = 0.5f;
+    // public float jumpForceMultiplier = 5f;
+    // private bool canJump = false;
+    // private float jumpTimer = 0f;
+    // private bool isFacingRight = true;
+    // public float bounceForce = 10f;
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
+    // [SerializeField] private Rigidbody2D rb;
+    // [SerializeField] private Transform groundCheck;
+    // [SerializeField] private LayerMask groundLayer;
+    // public float groundCheckRadius = 0.1f;
 
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            transform.Rotate(0f, 180f, 0f);
-        }
-    }
+    // private PlayerSoundJump playerSoundJump; // Reference to the PlayerSoundJump script
+
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    //     playerSoundJump = GetComponent<PlayerSoundJump>(); // Get the PlayerSoundJump script
+    // }
+
+    // void Update()
+    // {
+    //     horizontal = Input.GetAxisRaw("Horizontal");
+
+    //     // Update grounded status
+    //     bool isGroundedFlag = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    //     if (jumpTimer == 0.0f && isGroundedFlag)
+    //     {
+    //         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    //     }
+
+    //     // Reset canJump only when the player is grounded
+    //     if (isGroundedFlag && !canJump)
+    //     {
+    //         canJump = false;
+    //     }
+
+    //     // Start jump if grounded and space is pressed
+    //     if (Input.GetKeyDown(KeyCode.Space) && isGroundedFlag)
+    //     {
+    //         canJump = true;
+    //         jumpTimer = 0f;
+    //     }
+
+    //     if (Input.GetKey("space") && isGroundedFlag && canJump){
+    //         rb.velocity = new Vector2(0.0f, rb.velocity.y);
+    //     }
+
+    //     // Track jump hold time
+    //     if (Input.GetKey(KeyCode.Space) && canJump)
+    //     {
+    //         jumpTimer += Time.deltaTime;
+    //         jumpTimer = Mathf.Clamp(jumpTimer, 0f, maxJumpTime);
+    //     }
+
+    //     // Release jump and play sound
+    //     if (Input.GetKeyUp(KeyCode.Space) && canJump)
+    //     {
+    //         Jump();
+    //         playerSoundJump.PlayJumpSound(); // Play the jump sound
+    //         canJump = false;
+    //     }
+
+    //     Flip(); 
+    // }
+
+    // private void Jump()
+    // {
+    //     float totalJumpForce = jumpingPower + jumpForceMultiplier * (jumpTimer / maxJumpTime);
+    //     rb.AddForce(Vector3.up * totalJumpForce, ForceMode2D.Impulse);
+    // }
+
+    // // private void FixedUpdate()
+    // // {
+    // //     rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    // // }
+
+    // private void Flip()
+    // {
+    //     if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+    //     {
+    //         isFacingRight = !isFacingRight;
+    //         transform.Rotate(0f, 180f, 0f);
+    //     }
+    // }
 }
